a955874448521e6a7001401fc15a04c573a08a3a
Pasi Pyrrö <pasi.pyrro@aalto.fi>
2020-09-24 02:25:17 +0300

    fix hello world

--- input/input_hello_world.c
+++ input/input_hello_world.c
@@ -1,7 +1,5 @@
 /* I forgot to implement strings so this 
    should do for a "hello world program" */
-void fail(void) {
-    void foo; int bar;
-    bar = output(foo, 1234);
-    break;
+void main(void) {
+    output(1234);
 }
